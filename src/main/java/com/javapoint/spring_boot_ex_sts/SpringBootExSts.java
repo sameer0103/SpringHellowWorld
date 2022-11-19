7f34d0e9cc79d8d515af819b0061bb80a20a4567
Deshmukh <sameer.deshmukh2@dxc.com>
2022-11-19 17:57:26 +0530

    Added index.html

--- src/main/java/com/javapoint/spring_boot_ex_sts/SpringBootExSts.java
+++ src/main/java/com/javapoint/spring_boot_ex_sts/SpringBootExSts.java
@@ -1,13 +1,27 @@
 package com.javapoint.spring_boot_ex_sts;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;    
-import org.springframework.boot.autoconfigure.SpringBootApplication;   
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;   
 @SpringBootApplication   
 
 public class SpringBootExSts implements CommandLineRunner{
 	public static void main(String[] args)  
 	{    
-	SpringApplication.run(SpringBootExSts.class, args);    
+		String abc = "";
+		
+		
+		if(!StringUtils.isEmpty(abc))
+			
+		{
+			System.out.println("i am not null");
+			
+		}
+		else
+		{
+			System.out.println(" i dont have any value ");
+		}
+		SpringApplication.run(SpringBootExSts.class, args);    
 	}
 
 	@Override
